42bc83fca0e9674dfe533b7731c96c702449ed6c
RobbieKavanagh <130613407+RobbieKavanagh@users.noreply.github.com>
2023-06-07 21:12:22 -0230

    June 7 2023

---
+++ index.js
@@ -0,0 +1,8 @@
+function createNote() {
+const 
+ const newNote = document.createElement ("div");
+ const noteNumber = document.getElementById("note-container").childElementCount+1;
+ newNote.innerHTML = noteNumber.toString();
+ newNote.className = "note";
+ document.getElementById("note-container").append(newNote);
+}
