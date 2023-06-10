6394def1c2baa58a11fed9ff6a23a29469119567
RobbieKavanagh <130613407+RobbieKavanagh@users.noreply.github.com>
2023-06-10 08:10:15 -0230

    June 10 2023

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function createNote() {
- const newNote = document.createElement ("div");
+ const newNote = prompt("Enter New Note Here");
  const noteNumber = document.getElementById("note-container").childElementCount+1;
  newNote.innerHTML = noteNumber.toString();
  newNote.className = "note";
