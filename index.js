7002c8616a76286117de1638500fa6a30612f981
RobbieKavanagh <130613407+RobbieKavanagh@users.noreply.github.com>
2023-06-14 21:03:43 -0230

    June 14 2023

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function createNote() {
- const newNote = prompt("Enter New Note Here");
+ const noteContents = prompt("Enter New Note Here");
  const noteNumber = document.getElementById("note-container").childElementCount+1;
- newNote.innerHTML = noteNumber.toString();
+ const newNote = document.createElement("div");
+ const body = document.createElement("div");
+ newNote.innerHTML = noteNumber.toString()+noteContents;
+ body.id = "noteBody";
+ body.innerHTML = "testing";
+ newNote.appendChild(body);
  newNote.className = "note";
  document.getElementById("note-container").append(newNote);
 }
