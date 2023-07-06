c8853a3c1ec285967b6dafbafc66e05391c99e8b
RobbieKavanagh <130613407+RobbieKavanagh@users.noreply.github.com>
2023-07-06 19:11:18 -0230

    July 6 2023

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 function createNote() {
- const noteContents = prompt("Enter New Note Here");
- const noteNumber = document.getElementById("note-container").childElementCount+1;
- const newNote = document.createElement("div");
- newNote.className = "note";
- const body = document.createElement("div");
- newNote.innerHTML = noteNumber.toString()+noteContents;
+ const titleInput = prompt("Enter Note Title");
+ const bodyInput = prompt("Enter New Note Here");
 
- const noteTitle = document.createElement("div");
- noteTitle.innerHTML = noteNumber.toString()+noteContents;
- noteTitle.className = "noteTitle";
+ const noteDiv = document.createElement("div");
+ noteDiv.className = "note";
 
- body.id = "noteBody";
- body.innerHTML = "Testing";
- newNote.appendChild(body);
- newNote.className = "note";
- document.getElementById("note-container").append(newNote);
+ const titleDiv = document.getElementById("div");
+ titleDiv.className = "noteTitle";
+ 
 }
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Testing 123");
-    getNotes()
-});
