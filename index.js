018fb5e7fade51f51e1be449eeb23ccb94fee360
RobbieKavanagh <130613407+RobbieKavanagh@users.noreply.github.com>
2023-07-12 17:24:00 -0230

    July 12 2023

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function createNote() {
- const titleInput = prompt("Enter Note Title");
- const bodyInput = prompt("Enter New Note Here");
+    const titleInput = prompt("Enter Note Title");
+    const bodyInput = prompt("Enter New Note Here");
 
- const noteDiv = document.createElement("div");
- noteDiv.className = "note";
+    const noteDiv = document.createElement("div");
+    noteDiv.className = "note";
 
- const titleDiv = document.createElement("div");
- titleDiv.className = "noteTitle";
- titleDiv.innerHTML = titleInput;
+    const titleDiv = document.createElement("div");
+    titleDiv.className = "noteTitle";
+    titleDiv.innerHTML = titleInput;
 
- const bodyDiv = document.createElement("div");
- bodyDiv.className = "noteBody";
- bodyDiv.innerHTML = bodyInput;
+    const bodyDiv = document.createElement("div");
+    bodyDiv.className = "noteBody";
+    bodyDiv.innerHTML = bodyInput;
 
- noteDiv.appendChild(titleDiv);
- noteDiv.appendChild(bodyDiv);
+    noteDiv.appendChild(titleDiv);
+    noteDiv.appendChild(bodyDiv);
 
- document.getElementById("note-container").append(noteDiv);
+    document.getElementById("note-container").append(noteDiv);
 }
 
 function getNotes() {
@@ -24,6 +24,19 @@ function getNotes() {
     const noteDiv = document.createElement("div");
     noteDiv.className = "note";
 
-    
+    const titleDiv = document.createElement("div");
+    titleDiv.className = "noteTitle";
+    titleDiv.innerHTML = note.noteTitle;
+
+    const bodyDiv = document.createElement("div");
+    bodyDiv.className = "noteBody";
+    bodyDiv.innerHTML = note.noteBody;
+
+    const dateDiv = document.createElement("div");
+    dateDiv.className = "noteDate";
+    dateDiv.innerHTML = note.noteBody;
+
+    document.getElementById("note-container").append(noteDiv);
+
  });
 } 
